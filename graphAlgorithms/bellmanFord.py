74536168420fea1368738f81c7e7030e250ac161
Sandeep Chauhan <sandeepc701@gmail.com>
2021-11-13 12:24:36 +0530

    "Added code for negative weight cycle check"

--- graphAlgorithms/bellmanFord.py
+++ graphAlgorithms/bellmanFord.py
@@ -8,17 +8,29 @@ def bellmanFord(MAT,len1):
                 if dist[u]+MAT[u][v]<dist[v] :
                     dist[v]=dist[u]+MAT[u][v]
                     parent[v]=u;
-    print("Distance Vector {}".format(dist))
-    print("Parent Vector {} ".format(parent))
+    str1=str(dist)
+    for u in range(len1):  # This is Check for Negative Cycle
+        for v in range(len1):
+            if dist[u] + MAT[u][v] < dist[v]:
+                dist[v] = dist[u] + MAT[u][v]
 
-                
+    if not str1==str(dist):
+        print("\n\n Negative Cycle present in the graph !! Terminating .....")
+    else :
+        print("Distance Vector {}".format(dist))
+        print("Parent Vector {} ".format(parent))
 
-MAT=[
+
+'''MAT=[
 [0,3,INFINITY,7],
 [8,0,2,INFINITY],
 [5,INFINITY,0,1],
 [2,INFINITY,INFINITY,0]
-]
+] '''
+
+MAT=\
+[[0,-9,INFINITY],[INFINITY,0,2],[3,INFINITY,0]]
+
 len1 = len(MAT)
 dist = [(INFINITY if i > 0 else 0) for i in range(len1)]
 parent=[None for i in range(len1)]
