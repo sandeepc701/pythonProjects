c29c436d71dd401ebaa3fe537327a96cc8330c33
Sandeep Chauhan <sandeepc701@gmail.com>
2021-11-13 12:06:12 +0530

    "Added Parent Vector for tracking !!!!"

--- graphAlgorithms/bellmanFord.py
+++ graphAlgorithms/bellmanFord.py
@@ -1,15 +1,15 @@
 print("Welcome, this is the code for bellman ford algorithm !!!")
 INFINITY=9999999999999
 
-def bellmanFord(MAT):
-    len1=len(MAT)
-    dist=[(INFINITY if i >0 else 0) for i in range(len1)  ]
+def bellmanFord(MAT,len1):
     for i in range(len1-1):
         for u in range(len1):
             for v in range(len1):
                 if dist[u]+MAT[u][v]<dist[v] :
                     dist[v]=dist[u]+MAT[u][v]
-    print(dist)
+                    parent[v]=u;
+    print("Distance Vector {}".format(dist))
+    print("Parent Vector {} ".format(parent))
 
                 
 
@@ -19,4 +19,7 @@ MAT=[
 [5,INFINITY,0,1],
 [2,INFINITY,INFINITY,0]
 ]
-bellmanFord(MAT)
+len1 = len(MAT)
+dist = [(INFINITY if i > 0 else 0) for i in range(len1)]
+parent=[None for i in range(len1)]
+bellmanFord(MAT,len1)
